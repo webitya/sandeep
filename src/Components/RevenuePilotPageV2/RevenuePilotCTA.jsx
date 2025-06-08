ea2d1a8643f76b53df1d98f843754d9254d12ffc
adityawintech <aditya#19AI>
2025-06-08 14:42:49 +0530

    Aditya

--- src/Components/RevenuePilotPageV2/RevenuePilotCTA.jsx
+++ src/Components/RevenuePilotPageV2/RevenuePilotCTA.jsx
@@ -36,7 +36,7 @@ export default function RevenuePilotCTA() {
               href="/case-studies"
               className="border-2 border-white text-white px-8 py-4 rounded-lg font-semibold hover:bg-white hover:text-[#0A2540] transition"
             >
-              Download Success Case Study
+              Explore success case study 
             </a>
           </div>
         </motion.div>
